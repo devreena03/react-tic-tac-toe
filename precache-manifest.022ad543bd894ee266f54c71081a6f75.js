926b79dee78c182ea7ead2f325633f2d6172457f
Reena <rkreena07@gmail.com>
2019-06-14 10:55:04 +0530

    Updates

---
+++ precache-manifest.022ad543bd894ee266f54c71081a6f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d698cc4d3f04a284f2",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.87d698cc.js"
+  },
+  {
+    "revision": "5e325d10852f2aedc824",
+    "url": "/react-tic-tac-toe/static/js/main.5e325d10.chunk.js"
+  },
+  {
+    "revision": "af54f44fcb329bf3ab8f",
+    "url": "/react-tic-tac-toe/static/js/1.af54f44f.chunk.js"
+  },
+  {
+    "revision": "5e325d10852f2aedc824",
+    "url": "/react-tic-tac-toe/static/css/main.f8797936.chunk.css"
+  },
+  {
+    "revision": "9fcb1eac35fa6ab4b06a11124c25bb7f",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
